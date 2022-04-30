03ba05580a9a084765567fad62c025ba45fc7bdc
sry515 <sry0515@163.com>
2022-04-30 18:57:43 +0800

    tcp

--- cpp_server/tcp_client.c
+++ cpp_server/tcp_client.c
@@ -11,31 +11,32 @@
 void error_handling(const char *message);
 
 int main(int argc, const char *argv[]) {
-  int sock;
-  struct sockaddr_in serv_addr;
-  char message[30];
-  int str_len;
-
   if (argc != 3) {
     printf("Usage : %s <Ip> <port>", argv[0]);
     exit(1);
   }
 
-  sock = socket(PF_INET, SOCK_STREAM, 0);
+  int sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
   if (sock == -1) error_handling("socket() error");
 
+  struct sockaddr_in serv_addr;
   memset(&serv_addr, 0, sizeof(serv_addr));
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
-  serv_addr.sin_port = htons(atoi(argv[2]));
+  serv_addr.sin_port = htons(strtol(argv[2], NULL, 10));
 
-  if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1)
+  if (connect(sock, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) == -1)
     error_handling("connect() error");
 
-  str_len = read(sock, message, sizeof(message) - 1);
-  if (str_len == -1) error_handling("read() error");
+  int idx = 0, str_len = 0, read_len;
+  char message[30];
+  while (read_len = (int) read(sock, &message[idx++], 1)) {
+    if (read_len == -1) error_handling("read() error!");
+    str_len += read_len;
+  }
 
   printf("Message from serve : %s \n", message);
+  printf("Function read call count: %d\n", str_len);
   close(sock);
   return 0;
 }
--- cpp_server/tcp_server.c
+++ cpp_server/tcp_server.c
@@ -11,27 +11,20 @@
 void error_handling(const char *message);
 
 int main(int argc, char *argv[]) {
-  int serv_sock;
-  int clnt_sock;
-
-  struct sockaddr_in serv_addr;
-  struct sockaddr_in clnt_addr;
-  socklen_t clnt_addr_size;
-
-  const char message[] = "Hello World!";
   if (argc != 2) {
     printf("Usage : %s <port>\n", argv[0]);
     exit(1);
   }
 
   // create socket
-  serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+  int serv_sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
   if (serv_sock == -1) error_handling("socket() error");
 
+  struct sockaddr_in serv_addr;
   memset(&serv_addr, 0, sizeof(serv_addr));
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-  serv_addr.sin_port = htons(atoi(argv[1]));
+  serv_addr.sin_port = htons(strtol(argv[1], NULL, 10));
 
   // assignment Ip and port
   if (bind(serv_sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1)
@@ -41,11 +34,13 @@ int main(int argc, char *argv[]) {
   if (listen(serv_sock, 5) == -1) error_handling("listen() error");
 
   // handle connection request
-  clnt_addr_size = sizeof(clnt_addr);
-  clnt_sock = accept(serv_sock, (struct sockaddr *)&clnt_addr, &clnt_addr_size);
+  struct sockaddr_in clnt_addr;
+  socklen_t clnt_addr_size = sizeof(clnt_addr);
+  int clnt_sock = accept(serv_sock, (struct sockaddr *)&clnt_addr, &clnt_addr_size);
   if (clnt_sock == -1) error_handling("accept() error");
 
   // transmit data
+  char message[] = "Hello World";
   write(clnt_sock, message, sizeof(message));
   close(clnt_sock);
   close(serv_sock);
